fd00a41715aff66a65fb024477c1bafc90054569
easthobb <receiver@kakao.com>
2020-09-25 17:58:24 +0900

    REFACTOR

--- RPA_building.py
+++ RPA_building.py
@@ -1,36 +1,49 @@
 import openpyxl
 from openpyxl import load_workbook
 from openpyxl import Workbook
-
-
-## iteration 변수
-iter = 4
+from openpyxl.styles import Alignment
+import win32com.client
+import time
 
 ## 건물 양식 입력 자동화 python file
 
-# 엑셀파일 열기
-data = openpyxl.load_workbook('desktop/rpa/data.xlsx') #로 데이터 시트 오픈
-form = openpyxl.load_workbook('desktop/rpa/RPA_form.xlsx') #폼 데이터 시트 오픈
+###출력파일 및 excel PDF 파일 절대경로 설정
+filepath ="C:\\Users\\user\\Desktop\\RPA\\"
+input_image_format = '.png'
+saved_name = 'building'
 
-# 로 데이터 시트와 폼 시트의 fopen
-data_sheet = data.get_sheet_by_name('building') #로 데이터 시트 객체 파일(building,시트명 : building)
-form_sheet_page_1 = form.get_sheet_by_name('building_1') #폼 시트 페이지 1(building, 시트명 : building_1)
-form_sheet_page_2 = form.get_sheet_by_name('building_2') #폼 시트 페이지 2(building, 시트명 : building_2)
+### data sheet에서 필요한 설정요소 ### 
+data = openpyxl.load_workbook(filepath+'data.xlsx') #로 데이터 시트 오픈(변경가능)
+data_sheet_name = 'building' ## 데이터 파일 시트 
+start_column = 'A' ## 데이터 파일 시트에서 값을 가져올 시작 열
+end_column = 'AM' ## 데이터 파일 시트에서 값을 가져올 끝 열
+iter = 4 ## 문서 생성을 할 데이터의 갯수 
+data_sheet = data.get_sheet_by_name(data_sheet_name) #로 데이터 시트 객체 파일(land,시트명 : land)
 
-# (예정) for 문 삽입부분
+### form sheet 에서 필요한 설정요소
+form = openpyxl.load_workbook(filepath+'RPA_form.xlsx') #폼 데이터 시트 경로 지정(변경가능)
+form_sheet_list = ['building_1','building_2'] ## 접근할 시트(추가 및 삭제 가능)
+form_sheet_page_1 = form.get_sheet_by_name(form_sheet_list[0]) #폼 시트 페이지 1(building, 시트명 : land_1)
+form_sheet_page_2 = form.get_sheet_by_name(form_sheet_list[1]) #폼 시트 페이지 2(building, 시트명 : land_2)
+
+## 엑셀 PDF 연결
+excel = win32com.client.Dispatch('Excel.Application')
+excel.Visible = False
+
+
 # 로 데이터 개별 객체를 데이터 시트에서 추출
 for i in range(iter):
-    sheet_start_cell = "A" + str(i+iter)
-    sheet_end_cell = "AM" + str(i+iter)
+    sheet_start_cell = start_column + str(i+iter) 
+    sheet_end_cell = end_column + str(i+iter)
     
     building_object = data_sheet[sheet_start_cell:sheet_end_cell] # 반복시 변경해야 됩니다
     data_list = []
     for row in building_object:
         for cell in row:
             data_list.append(cell.value)
-
+    key_value = str(data_list[0])
     print(data_list)
-    # (예정) for 문 삽입부분
+    
     # 추출한 데이터를 양식에 매핑시켜주는 부분
     form_sheet_page_1['B2'] = data_list[0] ##관리번호
     form_sheet_page_1['D6'] = data_list[1] ##고유번호
@@ -59,7 +72,7 @@ for i in range(iter):
     # (예정) for문 삽입부분 
     # 이미지 입력부분 : 리사이즈 , 이미지 파일명 : 관리번호-1( ),관리번호-2( ), 관리번호-3( ),관리번호-4( )
     for j in range(4):
-        img_file_name = 'desktop/rpa/image/' + str(data_list[0]) +'-' +str(j+1) +'.png' # 첫번째 페이지 이미지 이름 지정
+        img_file_name = filepath + '\\image\\' +  key_value +'-' +str(j+1) + input_image_format # 첫번째 페이지 이미지 이름 지정
         print(img_file_name)
         img = openpyxl.drawing.image.Image(img_file_name)
         img.width=430 # 이미지 리사이징, 가로.픽셀 단위입니다.
@@ -93,23 +106,17 @@ for i in range(iter):
     form_sheet_page_2['J20'] = data_list[37] ## 의견
 
     #output file 저장 부분
-    output_file_name = 'desktop/rpa/output/building' + str(data_list[0]) +'.xlsx'
+    output_file_name = filepath + '\\output\\' + saved_name + key_value +'.xlsx' ## data_list[0] 관리번호
     form.save(output_file_name)
 
-    # sample_img.anchor(form_work_sheet.cell('B6'))
-    # form_work_sheet.add_image(sample_img,'B6')
-
-#form.save('desktop/rpa/output/output.xlsx')
+    #output file PDF 저장
+    wb = excel.WorkBooks.Open(output_file_name)
+    ws_chart = wb.WorkSheets(form_sheet_list)
+    ws_chart.Select()
+    pdf_save_path = filepath + "\\output\\" + key_value + '.pdf'
+    wb.ActiveSheet.ExportAsFixedFormat(0,pdf_save_path)
+    wb.Close(True)
 
 form.close()
 data.close()
-#work_sheet_new = work_book.create_sheet('new sheet')
-
-# work_sheet.rows는 해당 쉬트의 모든 행을 객체로 가지고 있음
-# for each_row in work_sheet.rows:
-#     # cell(row=행 번호, column=열 번호).value = 해당 세로셀/가로셀에 어떤 값을 넣어주세요
-#     work_sheet_new.cell(row=each_row[0].row, column=1).value = each_row[2].value
-    
-# 엑셀 파일 저장
-# work_book.save("desktop/rpa/output.xlsx")
-# work_book.close()
+excel.Quit()
--- RPA_land.py
+++ RPA_land.py
@@ -5,28 +5,36 @@ from openpyxl.styles import Alignment
 import win32com.client
 import time
 
-## iteration 변수
-iter = 4
+## 토지 양식 입력 자동화 python file
 
-## 건물 양식 입력 자동화 python file
+###출력파일 및 excel PDF 파일 절대경로 설정
+filepath ="C:\\Users\\user\\Desktop\\RPA\\"
+input_image_format = '.png'
+saved_name = 'land'
 
-# 엑셀파일 열기
+### data sheet에서 필요한 설정요소 ### 
+data = openpyxl.load_workbook(filepath+'data.xlsx') #로 데이터 시트 오픈(변경가능)
+data_sheet_name = 'land' ## 데이터 파일 시트 
+start_column = 'A' ## 데이터 파일 시트에서 값을 가져올 시작 열
+end_column = 'AP' ## 데이터 파일 시트에서 값을 가져올 끝 열
+iter = 4 ## 문서 생성을 할 데이터의 갯수 
+data_sheet = data.get_sheet_by_name(data_sheet_name) #로 데이터 시트 객체 파일(land,시트명 : land)
+
+### form sheet 에서 필요한 설정요소
+form = openpyxl.load_workbook(filepath+'RPA_form.xlsx') #폼 데이터 시트 경로 지정(변경가능)
+form_sheet_list = ['land_1','land_2','land_3'] ## 접근할 시트(추가 및 삭제 가능)
+form_sheet_page_1 = form.get_sheet_by_name(form_sheet_list[0]) #폼 시트 페이지 1(building, 시트명 : land_1)
+form_sheet_page_2 = form.get_sheet_by_name(form_sheet_list[1]) #폼 시트 페이지 2(building, 시트명 : land_2)
+form_sheet_page_3 = form.get_sheet_by_name(form_sheet_list[2]) #폼 시트 페이지 3(building, 시트명 : land_3)
+
+## 엑셀 PDF 연결
 excel = win32com.client.Dispatch('Excel.Application')
 excel.Visible = False
 
-data = openpyxl.load_workbook('desktop/rpa/data.xlsx') #로 데이터 시트 오픈
-form = openpyxl.load_workbook('desktop/rpa/RPA_form.xlsx') #폼 데이터 시트 오픈
-
-# 로 데이터 시트와 폼 시트의 fopen
-data_sheet = data.get_sheet_by_name('land') #로 데이터 시트 객체 파일(land,시트명 : land)
-form_sheet_page_1 = form.get_sheet_by_name('land_1') #폼 시트 페이지 1(building, 시트명 : land_1)
-form_sheet_page_2 = form.get_sheet_by_name('land_2') #폼 시트 페이지 2(building, 시트명 : land_2)
-form_sheet_page_3 = form.get_sheet_by_name('land_3') #폼 시트 페이지 3(building, 시트명 : land_3)
-
 # 로 데이터 개별 객체를 데이터 시트에서 추출
 for i in range(iter): 
-    sheet_start_cell = "A" + str(i+iter) 
-    sheet_end_cell = "AP" + str(i+iter)
+    sheet_start_cell = start_column + str(i+iter) 
+    sheet_end_cell = end_column + str(i+iter)
     
     land_object = data_sheet[sheet_start_cell:sheet_end_cell] # 반복시 변경해야 됩니다
     data_list = [] ## 개별 토지 객체의 정보가 담기는 리스트 생성
@@ -34,7 +42,9 @@ for i in range(iter):
         for cell in row:
             data_list.append(cell.value)
 
-    print(data_list)
+    print(data_list) # 각 행의 값들을 콘솔에 표시
+    key_value = str(data_list[0]) #넘버링을 위한 key value 엑셀에 첫번째 행에 해당합니다. , 예상 정수값
+    
     # (예정) for 문 삽입부분
     # 추출한 데이터를 양식에 매핑시켜주는 부분 :  land_1 sheet
     form_sheet_page_1['B1'] = data_list[0] ##관리번호
@@ -90,7 +100,7 @@ for i in range(iter):
     # 이미지 입력부분 : 리사이즈 , 이미지 파일명 : 관리번호-1( ),관리번호-2( ), 관리번호-3( ),관리번호-4( ), 관리번호-5( ),
     # (향후 변경) 1 : 지적도 , 2 : 국토정보기본도, 3 : 현황사진, 4 :사용허가및무단점유현황, 5 : 토지이용계획확인서 
     for j in range(5):
-        img_file_name = 'desktop/rpa/image/' + str(data_list[0]) +'-' +str(j+1) +'.png' # 첫번째 페이지 이미지 이름 지정
+        img_file_name = filepath + '\\image\\' +  key_value +'-' +str(j+1) + input_image_format # 첫번째 페이지 이미지 이름 지정
         print(img_file_name)
         img = openpyxl.drawing.image.Image(img_file_name)
         if(j==0): # 지적도
@@ -105,8 +115,6 @@ for i in range(iter):
             img.width=430 # 이미지 리사이징, 가로.픽셀 단위입니다.
             img.height=286 # 이미지 리사이징 세로.픽셀 단위입니다.
             form_sheet_page_1.add_image(img,'E34')
-            cell = form_sheet_page_1.cell(row=12,column=13)
-            cell.alignment = Alignment(horizontal='center')
         elif(j==3): # 사용허가및무단점유현황
             img.width=864 # 이미지 리사이징, 가로.픽셀 단위입니다.
             img.height=400 # 이미지 리사이징 세로.픽셀 단위입니다.
@@ -117,34 +125,17 @@ for i in range(iter):
             form_sheet_page_3.add_image(img,'B3')
 
     #output file 저장 부분
-    output_file_name = 'desktop/rpa/output/land' + str(data_list[0]) +'.xlsx'
+    output_file_name = filepath + '\\output\\' + saved_name + key_value +'.xlsx' ## data_list[0] 관리번호
     form.save(output_file_name)
-    time.sleep(1)
+
     #output file PDF 저장
-    output_file_root = "C:\\Users\\user\\Desktop\\RPA\\output\\land" + str(data_list[0]) + '.xlsx' 
-    wb = excel.WorkBooks.Open(output_file_root)
-    ws_chart = wb.WorkSheets(['land_1','land_2','land_3'])
+    wb = excel.WorkBooks.Open(output_file_name)
+    ws_chart = wb.WorkSheets(form_sheet_list)
     ws_chart.Select()
-    pdf_save_path = "C:\\Users\\user\\Desktop\\RPA\\output\\" + str(data_list[0]) + '.pdf'
+    pdf_save_path = filepath + "\\output\\" + key_value + '.pdf'
     wb.ActiveSheet.ExportAsFixedFormat(0,pdf_save_path)
     wb.Close(True)
-    
-    # sample_img.anchor(form_work_sheet.cell('B6'))
-    # form_work_sheet.add_image(sample_img,'B6')
-
-#form.save('desktop/rpa/output/output.xlsx')
 
 form.close()
 data.close()
 excel.Quit()
-
-#work_sheet_new = work_book.create_sheet('new sheet')
-
-# work_sheet.rows는 해당 쉬트의 모든 행을 객체로 가지고 있음
-# for each_row in work_sheet.rows:
-#     # cell(row=행 번호, column=열 번호).value = 해당 세로셀/가로셀에 어떤 값을 넣어주세요
-#     work_sheet_new.cell(row=each_row[0].row, column=1).value = each_row[2].value
-    
-# 엑셀 파일 저장
-# work_book.save("desktop/rpa/output.xlsx")
-# work_book.close()
