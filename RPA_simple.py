4cade6e304210be127dac4d123afc2e8833a6da7
easthobb <receiver@kakao.com>
2020-09-29 17:47:36 +0900

    debug complete

--- RPA_simple.py
+++ RPA_simple.py
@@ -3,7 +3,8 @@ from openpyxl import load_workbook
 from openpyxl import Workbook
 from openpyxl.styles import Alignment
 import win32com.client
-import time
+import PIL
+
 
 ## 토지 양식 입력 자동화 python file
 
@@ -20,20 +21,27 @@ end_column = 'Q' ## 데이터 파일 시트에서 값을 가져올 끝 열
 iter = 5 ## 문서 생성을 할 데이터의 갯수
 data_sheet = data.get_sheet_by_name(data_sheet_name) #로 데이터 파일(resultDB.xlxs,시트명 : '시계외' or '시내')
 
-### form sheet 에서 필요한 설정요소
-form = openpyxl.load_workbook(filepath+'RPA_form.xlsx') #폼 데이터 시트 경로 지정(변경가능)
-form_sheet_list = ['simple'] ## 접근할 시트(추가 및 삭제 가능)
-form_sheet_page_1 = form.get_sheet_by_name(form_sheet_list[0]) #폼 시트 페이지 1(simple, 시트명 : simple)
-#form_sheet_page_2 = form.get_sheet_by_name(form_sheet_list[1]) #폼 시트 페이지 2(building, 시트명 : land_2)
-#form_sheet_page_3 = form.get_sheet_by_name(form_sheet_list[2]) #폼 시트 페이지 3(building, 시트명 : land_3)
 
-## 엑셀 PDF 연결
+
+
+
+## 엑셀 PDF 연결, background 실행
 excel = win32com.client.Dispatch('Excel.Application')
 excel.Visible = False
+excel.ScreenUpdating = False
+excel.DisplayAlerts = False
+excel.EnableEvents = False
 
 # 로 데이터 개별 객체를 데이터 시트에서 추출
-for i in range(iter): 
-    sheet_start_cell = start_column + str(i+4) ## 하드코딩 시작 데이터 셀 A4
+for i in range(iter):
+
+    ### form sheet 에서 필요한 설정요소
+    form = openpyxl.load_workbook(filepath+'RPA_form.xlsx') #폼 데이터 시트 경로 지정(변경가능)
+    form_sheet_list = ['simple'] ## 접근할 시트(추가 및 삭제 가능)
+    form_sheet_page_1 = form.get_sheet_by_name(form_sheet_list[0]) #폼 시트 페이지 1(simple, 시트명 : simple)
+    
+    ### 시작셀과 마지막 셀 설정 
+    sheet_start_cell = start_column + str(i+4) ## 하드코딩 시작 데이터 셀 ex) A4 -> i = 4 , A7 -> i = 7
     sheet_end_cell = end_column + str(i+4)
     
     land_object = data_sheet[sheet_start_cell:sheet_end_cell] # 반복시 변경해야 됩니다
@@ -72,15 +80,14 @@ for i in range(iter):
     for j in range(3):
         img_file_name = filepath + '\\image\\' + '\\simple\\' + key_value +'_' +str(j+1) + input_image_format # 이미지경로 
         print(img_file_name)
-        try:##이미지 파일 삽입시도.
+        try: ##이미지 파일 삽입시도.
             img = openpyxl.drawing.image.Image(img_file_name)
-        except: ##해당하는 이미지 파일이 없을경우
-            print('수임번호'+key_value+'에 해당하는 이미지 파일이 존재하지 않습니다.')
+        except: ##해당하는 이미지 파일이 없을경우, form overriding 방지
+            print('수임번호'+ key_value +'에 해당하는 이미지 파일이 존재하지 않습니다.')
         else:
             if(j==0): # 지적도
                 img.width=432 # 이미지 리사이징, 가로.픽셀 단위입니다.
                 img.height=286 # 이미지 리사이징 세로.픽셀 단위입니다.
-                print('img insertion!')
                 form_sheet_page_1.add_image(img,'B18') ## 이미지가 들어갈 셀
             elif(j==1): # 국토정보기본도
                 img.width=432 # 이미지 리사이징, 가로.픽셀 단위입니다.
@@ -95,15 +102,17 @@ for i in range(iter):
     #output file 저장 부분
     output_file_name = filepath + '\\output\\' + saved_name + key_value +'.xlsx' ## data_list[10] 수임번호
     form.save(output_file_name)
+    form.close()
 
     #output file PDF 저장
+    excel.Visible = False
     wb = excel.WorkBooks.Open(output_file_name)
+    excel.Visible = False
     ws_chart = wb.WorkSheets(form_sheet_list)
     ws_chart.Select()
     pdf_save_path = filepath + "\\output\\" +saved_name + key_value + '.pdf'
     wb.ActiveSheet.ExportAsFixedFormat(0,pdf_save_path)
     wb.Close(True)
 
-form.close()
 data.close()
 excel.Quit()
